ee47cf2d64f609b84e70133603a4e605b675f5ad
Kari <Kari@192.168.1.100>
2019-09-18 11:04:59 +0300

    lisätty perus Core-objekti

---
+++ markkinasim/src/test/java/otp/markkinasim/modeltest/CoreTest.java
@@ -0,0 +1,17 @@
+package otp.markkinasim.modeltest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import model.Core;
+
+import org.junit.jupiter.api.Test;
+
+class CoreTest {
+
+	private Core core = Core.getInstance();
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
