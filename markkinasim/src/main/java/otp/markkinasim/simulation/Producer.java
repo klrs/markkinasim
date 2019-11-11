49df5a7640342685b19e8d2d3fc14fbb25c9c9ea
klrs <kllrssn@gmail.com>
2019-11-11 19:07:09 +0200

    refaktoroitu inventory & lisätty partyn aliluokat

---
+++ markkinasim/src/main/java/otp/markkinasim/simulation/Producer.java
@@ -0,0 +1,16 @@
+package otp.markkinasim.simulation;
+
+import javax.persistence.Transient;
+
+public class Producer extends Party {
+	public Producer() {
+		super();
+	}
+	
+	@Override
+	public void produce() {
+		double producedAmount = checkProducedAmount();
+		producedItemInventory.addAmount((int)producedAmount);
+		calculateRemainder(producedAmount);
+	}
+}
