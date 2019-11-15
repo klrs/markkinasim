adb478dae9d1526ea84afcc308532f2b73dbdefd
Dani Kuikka <dani.kuikka@metropolia.fi>
2019-11-15 12:08:49 +0200

    new methods for Accountant

--- markkinasim/src/main/java/log/Accountant.java
+++ markkinasim/src/main/java/log/Accountant.java
@@ -2,26 +2,42 @@ package log;
 import java.util.ArrayList;
 
 public class Accountant {
-	//public static final Accountant INSTANCE = new Accountant();
 	
-	//public Accountant() {}
-	
-	ArrayList<LogObject> log = new ArrayList<LogObject>();
+	ArrayList<LogObject> completeLog = new ArrayList<LogObject>();
+	ArrayList<LogObject> tradeLog = new ArrayList<LogObject>();
 	
 	public void registerTrade(TradeLog trade) {
-		log.add(trade);
+		completeLog.add(trade);
+		tradeLog.add(trade);
 	};
 	
+	public ArrayList<LogObject> getCompleteLog() {
+		return completeLog;
+	}
+	
 	public ArrayList<LogObject> getAllTrades() {
-		return log;
+		return tradeLog;
 	}
 	
-	/*
-	public static Accountant getInstance() {
-		return INSTANCE;
+	public ArrayList<LogObject> getTradesForDay(int day) {
+		ArrayList<LogObject> logOneDay = new ArrayList<LogObject>();
+		for (LogObject object : tradeLog) {
+			if (object.getDay() == day) {
+				logOneDay.add(object);
+			}
+		}
+		return logOneDay;
 	}
-	*/
 	
+	public ArrayList<LogObject> getCompleteLogForDay(int day) {
+		ArrayList<LogObject> logOneDay = new ArrayList<LogObject>();
+		for (LogObject object : completeLog) {
+			if (object.getDay() == day) {
+				logOneDay.add(object);
+			}
+		}
+		return logOneDay;
+	}
 	
 	
 }
